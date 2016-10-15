324747542b0fbb00486c0377a74816decc5a9f19
Sarah Wong <sarah.wong.0x00@gmail.com>
2016-10-15 12:41:51 -0400

    yapf format

--- webcontrol/webcontrol.py
+++ webcontrol/webcontrol.py
@@ -5,17 +5,18 @@ import json
 
 class SarahsDesk(object):
     def put(self, data):
-        return {"reply" : data}
+        return {"reply": data}
 
-OBJECTS = {
-    "/sarahsdesk" : SarahsDesk(),
-}
+
+OBJECTS = {"/sarahsdesk": SarahsDesk(), }
+
 
 class WebControl(http.server.SimpleHTTPRequestHandler):
     def do_PUT(self):
         if self.path in OBJECTS:
             size = int(self.headers.get("content-length"))
-            output = OBJECTS[self.path].put(json.loads(self.rfile.read(size).decode("UTF-8")))
+            output = OBJECTS[self.path].put(
+                json.loads(self.rfile.read(size).decode("UTF-8")))
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
@@ -24,7 +25,7 @@ class WebControl(http.server.SimpleHTTPRequestHandler):
         else:
             self.send_response(404)
 
+
 if __name__ == "__main__":
-    httpd = http.server.HTTPServer(('', 8080),
-                                   WebControl)
+    httpd = http.server.HTTPServer(('', 8080), WebControl)
     httpd.serve_forever()
